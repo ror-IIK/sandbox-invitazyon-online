0ae820b22fed0250cd6c05abea7c9b8ed61fa1c1
Thiago Moreno <thiago.roriik@gmail.com>
2026-02-11 17:33:54 -0600

    Estructura inicial de landing_page invitazyon

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Esperar a que el DOM cargue
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Invitazyon.online Landing lista 🚀');
+
+    // Aquí pondremos la lógica del menú móvil más adelante
+    // y las animaciones de scroll.
+});
